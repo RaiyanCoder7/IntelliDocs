e7858539054cc1ac000092f712648af4fa352fe4
md raiyan raza khan <mdraiyanrazakhan@gmail.com>
2025-10-18 19:10:51 +0530

    Updated frontend UI and fixed minor bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import styles from './App.module.css';
 import './index.css';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 
 // Home component - defined here since we don't have a separate Home.jsx yet
 function Home() {
